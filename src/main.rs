26f4a3a0e7133ddaf5fa30b7ab1a2b4a84bda07d
Vadim Slynko <vadim@slynko.rocks>
2020-07-29 08:44:00 +0200

    logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#[macro_use]
+extern crate log;
+extern crate simple_logger;
+
 fn main() {
+    simple_logger::init().expect("Cannot initialize logging");
+    info!("Grab images v");
     println!("Hello, world!");
 }
