7e071591500b631b74497750632bcac740b778bd
Vadim Slynko <vadim@slynko.rocks>
2020-08-01 19:34:45 +0200

    prepare for some tests

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,30 @@ extern crate simple_logger;
 
 use std::error::Error;
 use std::fs;
+
 use std::path::PathBuf;
 use structopt::StructOpt;
 
+const FILE_TYPES_GLOB: &'static str = "*.jpg";
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+fn unwind_glob_sources(sources: Vec<&str>) -> Vec<PathBuf> {
+    let mut result = Vec::new();
+    for source_dir in &sources {
+        info!("Source: {}", source_dir);
+        let walker = globwalk::GlobWalkerBuilder::new(source_dir, FILE_TYPES_GLOB)
+            .case_insensitive(true)
+            .build()
+            .unwrap()
+            .into_iter()
+            .filter_map(Result::ok);
+        for img in walker {
+            result.push(img.into_path());
+        }
+    }
+
+    result
+}
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "Grab Images", about = "Grab and dedublicate images.")]
@@ -21,24 +41,16 @@ struct Opt {
 fn main() -> Result<(), Box<dyn Error>> {
     simple_logger::init().expect("Cannot initialize logging");
     let opt = Opt::from_args();
-    println!("{:?}", opt);
+    debug!("{:?}", opt);
     info!("Grab Images version {}", VERSION);
 
     let input_txt = fs::read_to_string(opt.input_list).unwrap();
     let lines: Vec<&str> = input_txt.lines().collect();
 
-    for line in &lines {
-        println!("Input line {}", line);
-    }
+    let all_sources = unwind_glob_sources(lines);
 
-    let walker = globwalk::GlobWalkerBuilder::from_patterns("C:\\", &lines)
-    // let walker = globwalk::GlobWalkerBuilder::from_patterns("C:\\", &["*.txt"])
-        .build()?
-        .into_iter()
-        .filter_map(Result::ok);
-
-    for img in walker {
-        println!("Line {:?}", img.path());
+    for source in all_sources {
+        info!("Image: {:?}", source);
     }
 
     return Ok(());
